37e468a36e299b5129a422253bfc6ff4f7a79c8a
Mish Ushakov <mishushakov@users.noreply.github.com>
2020-04-24 15:32:27 +0200

    http cloning

--- gh-clone.sh
+++ gh-clone.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sS -u $GITHUB_USERNAME:$GITHUB_TOKEN 'https://api.github.com/user/repos?per_page=10000' | jq -r '.[] .ssh_url' | sed -e 's/.*:\(.*\)\..*/\1/' | xargs -I{} git clone git@github.com:{}.git /tmp/gh-clone/{}; mv /tmp/gh-clone/* $1/; rm -r /tmp/gh-clone
+curl -sS -u $GITHUB_USERNAME:$GITHUB_TOKEN 'https://api.github.com/user/repos?per_page=10000' | jq -r '.[] .ssh_url' | sed -e 's/.*:\(.*\)\..*/\1/' | xargs -I{} git clone https://$GITHUB_TOKEN@github.com/{}.git /tmp/gh-clone/{}; mv /tmp/gh-clone/* $1/; rm -r /tmp/gh-clone
